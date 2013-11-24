fed09f31b091283c8f0dfae1f644685f8525b5e2
montymxb <friedman.benjamin@gmail.com>
2013-11-23 21:21:47 -0800

    baked beans edition

--- killLaunch.sh
+++ killLaunch.sh
@@ -1,3 +1,8 @@
+##Kills launch.sh
 ps -A | grep "launch.sh" | awk '{print $1}' | xargs -r  kill
-ps -A | grep "anotherThing.sh" | awk '{print $1}' | xargs -r kill
+
+##Kills All Server Sessions (ideally)
 screen -ls | grep "mineBumbs.*Detached" | awk '{print $1}' | xargs -n1 -i screen -X -S {} quit
+
+##Kills vestigial servers
+ps -A | grep "java" | awk '{print $1}' | xargs -r  kill
--- launch.sh
+++ launch.sh
@@ -8,13 +8,12 @@ cd "$(dirname "$0")"
 
 serverSession() {
 
+	#restart every 4 hours?? 14400 sleep time total required
+
+	#cycles = total hours between restarts
 	cycles=4
 	hour=3600
 	period=$((cycles*hour))
-
-	screen -S mineBumbs java -Xmx1024m -Xms1024m -jar minecraft_server.jar nogui
-
-	#restart every 4 hours?? 14400 sleep time total required
 
 	sleep 80
 
@@ -30,10 +29,11 @@ serverSession() {
 
 	screen -S mineBumbs -X stuff "say compliments from Superkelp and mxb..."
 	screen -S mineBumbs -X eval "stuff \015"
+	
 
 	sleep $((period-100-600))
 
-
+#	####Future feauture: ping each hour
 #	while [ "$cycles" -gt 1 ]; do
 #	currentHour=$((period/3600+1-cycles))
 #	screen -S mineBumbs -X stuff "say Server uptime of $currentHour hour..."
@@ -74,17 +74,21 @@ serverSession() {
 
 	sleep 30
 
+	##### screen kill OSX Style
 	#Kill the screen
-	#screen -S mineBumbs -X stuff "exit"
-	#screen -S mineBumbs -X eval "stuff \015"
-
-	screen -ls | grep "mineBumbs" | awk '{print $1}' | xargs -i -n1 screen -X -S {} quit
+	screen -S mineBumbs -X stuff "exit"
+	screen -S mineBumbs -X eval "stuff \015"
+	####
 }
 
 while true; do
 
-	serverSession
+	serverSession &
+	
+	screen -S mineBumbs java -Xmx1024m -Xms1024m -jar minecraft_server.jar nogui	
 
-	screen -ls | grep "mineBumbs" | awk '{print $1}' | xargs -i -n1 screen -X -S {} quit
+	screen -ls | grep "mineBumbs" | awk '{print $1}' | xargs -r -i -n1 screen -X -S {} quit
+	
+	sleep 10
 
 done
